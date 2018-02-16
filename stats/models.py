bb36482b26c026cea9894d223ea56dd1c7068505
Dmitriy Zuykov <dmitriy.zuykov@altarix.org>
2018-02-16 17:37:25 +0400

    reformat

--- stats/models.py
+++ stats/models.py
@@ -1,130 +1,138 @@
 from django.db import models, connection
 import datetime
 
+
 class TradePoint(models.Model):
-	class Meta:
-		verbose_name = 'Торговая точка'
-		verbose_name_plural = 'Тороговые точки'
-	name = models.CharField("Торговая точка",max_length=200)
-	region = models.CharField("Регион",max_length=200)
-	city = models.CharField("Город",max_length=200)
-	address = models.CharField("Адрес",max_length=600)
-	open_time = models.TimeField("Время открытия",default=datetime.time(9,0,0))
-	close_time = models.TimeField("Время закрытия",default=datetime.time(22,0,0))
-	target_count = models.IntegerField('Количество мишеней', default=1)
-	label = models.IntegerField('ID', default=1)
-	capacity = models.IntegerField('Вместимость', default=0)
-	remaining_amount = models.FloatField('Оставшееся количество', default=0)
-	big_toys_count = models.IntegerField('Большие игрушки', default=0)
-	small_toys_count = models.IntegerField('Маленькие игрушки', default=0	)
+    class Meta:
+        verbose_name = 'Торговая точка'
+        verbose_name_plural = 'Тороговые точки'
 
-	def __str__(self):
-		return self.name
+    name = models.CharField("Торговая точка", max_length=200)
+    region = models.CharField("Регион", max_length=200)
+    city = models.CharField("Город", max_length=200)
+    address = models.CharField("Адрес", max_length=600)
+    open_time = models.TimeField("Время открытия", default=datetime.time(9, 0, 0))
+    close_time = models.TimeField("Время закрытия", default=datetime.time(22, 0, 0))
+    target_count = models.IntegerField('Количество мишеней', default=1)
+    label = models.IntegerField('ID', default=1)
+    capacity = models.IntegerField('Вместимость', default=0)
+    remaining_amount = models.FloatField('Оставшееся количество', default=0)
+    big_toys_count = models.IntegerField('Большие игрушки', default=0)
+    small_toys_count = models.IntegerField('Маленькие игрушки', default=0)
 
-	def filteredGameSetCount(self, **kvargs):
-		return self.game_set.filter(**kvargs).count()
+    def __str__(self):
+        return self.name
 
-	def status(self):
-		return self.filteredGameSetCount(start_time__range = [
-			datetime.datetime.now() - datetime.timedelta(minutes=30), datetime.datetime.now()
-			]) > 0
-	def game_count(self):
-		return self.filteredGameSetCount(start_time__range = [
-				datetime.date.today(), datetime.datetime.now()
-			])
+    def filteredGameSetCount(self, **kvargs):
+        return self.game_set.filter(**kvargs).count()
 
-	def interruptions(self):
-		interruptCount = self.filteredGameSetCount(start_time__range = [
-				datetime.date.today(), datetime.datetime.now()
-			], interrupt=False)
-		if interruptCount == 0:
-			return  "Нет"
-		return interruptCount
+    def status(self):
+        return self.filteredGameSetCount(start_time__range=[
+            datetime.datetime.now() - datetime.timedelta(minutes=30), datetime.datetime.now()
+        ]) > 0
 
-	def bulletsCount(self):
-		with connection.cursor() as cursor:
-			cursor.execute("select sum(bullet_count) from stats_bullethistory bh where bh.trade_point_id = %s", [self.id])
-			result = cursor.fetchone()
-		return result[0]
+    def game_count(self):
+        return self.filteredGameSetCount(start_time__range=[
+            datetime.date.today(), datetime.datetime.now()
+        ])
 
-	# def bulletsLeft(self):
-	# 	return 'nan'
-	# 	if self.capacity == 0:
-	# 		return 'Не определено'
-	# 	percentage = self.remaining_amount * 100 / self.capacity
-	# 	if percentage > 100:
-	# 		return '100%'
-	# 	if percentage < 1:
-	# 		return '0%'
-	# 	return "{0:.1f}%".format(percentage)
+    def interruptions(self):
+        interruptCount = self.filteredGameSetCount(start_time__range=[
+            datetime.date.today(), datetime.datetime.now()
+        ], interrupt=False)
+        if interruptCount == 0:
+            return "Нет"
+        return interruptCount
 
-	status.short_description = 'Работает'
-	status.boolean = True
+    def bulletsCount(self):
+        with connection.cursor() as cursor:
+            cursor.execute("select sum(bullet_count) from stats_bullethistory bh where bh.trade_point_id = %s",
+                           [self.id])
+            result = cursor.fetchone()
+        return result[0]
 
-	game_count.short_description = 'Количество игр'
+    # def bulletsLeft(self):
+    # 	return 'nan'
+    # 	if self.capacity == 0:
+    # 		return 'Не определено'
+    # 	percentage = self.remaining_amount * 100 / self.capacity
+    # 	if percentage > 100:
+    # 		return '100%'
+    # 	if percentage < 1:
+    # 		return '0%'
+    # 	return "{0:.1f}%".format(percentage)
 
-	interruptions.short_description = 'Прерывание'
+    status.short_description = 'Работает'
+    status.boolean = True
 
-	bulletsCount.short_description = 'Пули'
+    game_count.short_description = 'Количество игр'
+
+    interruptions.short_description = 'Прерывание'
+
+    bulletsCount.short_description = 'Пули'
 
 
 class BulletHistory(models.Model):
-	class Meta:
-		indexes = [
-			models.Index(fields=['trade_point'], name='bullet_history_tp_idx')
-		]
-		verbose_name = 'Приход пуль'
-		verbose_name_plural = 'Приходы пуль'
-	trade_point = models.ForeignKey(TradePoint, on_delete=models.CASCADE)
-	bullet_count = models.IntegerField('Приход пуль', default=1)
-	start_time = models.DateTimeField('Время прихода', default=datetime.datetime.now)
+    class Meta:
+        indexes = [
+            models.Index(fields=['trade_point'], name='bullet_history_tp_idx')
+        ]
+        verbose_name = 'Приход пуль'
+        verbose_name_plural = 'Приходы пуль'
+
+    trade_point = models.ForeignKey(TradePoint, on_delete=models.CASCADE)
+    bullet_count = models.IntegerField('Приход пуль', default=1)
+    start_time = models.DateTimeField('Время прихода', default=datetime.datetime.now)
 
 
 class Operator(models.Model):
-	class Meta:
-		verbose_name = 'Оператор'
-		verbose_name_plural = 'Операторы'
-	name = models.CharField(max_length=200)
-	trade_point = models.ForeignKey(TradePoint, on_delete=models.CASCADE)
-	start_time = models.TimeField("Начало работы",default=datetime.time(9,0,0))
-	end_time = models.TimeField("Конец работы",default=datetime.time(22,0,0))
-	phone = models.CharField("Телефон", max_length=200)
+    class Meta:
+        verbose_name = 'Оператор'
+        verbose_name_plural = 'Операторы'
+
+    name = models.CharField(max_length=200)
+    trade_point = models.ForeignKey(TradePoint, on_delete=models.CASCADE)
+    start_time = models.TimeField("Начало работы", default=datetime.time(9, 0, 0))
+    end_time = models.TimeField("Конец работы", default=datetime.time(22, 0, 0))
+    phone = models.CharField("Телефон", max_length=200)
 
 
 class GameType(models.Model):
-	pass
+    pass
 
 
 class Target(models.Model):
-	class Meta:
-		verbose_name = "Мишень"
-		verbose_name_plural = "Мишени"
-	trade_point = models.ForeignKey(TradePoint, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Мишень"
+        verbose_name_plural = "Мишени"
+
+    trade_point = models.ForeignKey(TradePoint, on_delete=models.CASCADE)
 
 
-#Точка тира`, `Мишень`, `Дата`, `Время`, `Время игры`, `Статистика упавших`, `Количество холостых`, `Приз`, `Прерывание`, `Служебные`) VALUES ('63001001', '5', '2017-02-01', '14:03:12', '99', '11010111010100100010', '1', '0', '0', '0');
+# Точка тира`, `Мишень`, `Дата`, `Время`, `Время игры`, `Статистика упавших`, `Количество холостых`, `Приз`, `Прерывание`, `Служебные`) VALUES ('63001001', '5', '2017-02-01', '14:03:12', '99', '11010111010100100010', '1', '0', '0', '0');
 class Game(models.Model):
-	class Meta:
-		verbose_name = "Игра"
-		verbose_name_plural = "Статистика"
-	trade_point = models.ForeignKey(TradePoint, on_delete=models.CASCADE)
-	target = models.IntegerField("Мишень")
-	start_time = models.DateTimeField('Время начала')
-	duration = models.DurationField('Продолжительность игры', default=datetime.timedelta(0))
-	fallen_count = models.CharField("Упавшие", max_length=200)
-	idle_count = models.IntegerField("Холостые")
-	prise = models.BooleanField("Приз")
-	interrupt = models.BooleanField("Прерывание")
-	service = models.IntegerField("Служебные")
+    class Meta:
+        verbose_name = "Игра"
+        verbose_name_plural = "Статистика"
 
+    trade_point = models.ForeignKey(TradePoint, on_delete=models.CASCADE)
+    target = models.IntegerField("Мишень")
+    start_time = models.DateTimeField('Время начала')
+    duration = models.DurationField('Продолжительность игры', default=datetime.timedelta(0))
+    fallen_count = models.CharField("Упавшие", max_length=200)
+    idle_count = models.IntegerField("Холостые")
+    prise = models.BooleanField("Приз")
+    interrupt = models.BooleanField("Прерывание")
+    service = models.IntegerField("Служебные")
 
-	def __str__(self):
-		return 'Игра'
+    def __str__(self):
+        return 'Игра'
 
 
 class Error(models.Model):
-	class Meta:
-		verbose_name = 'Ошибка'
-		verbose_name_plural = 'Ошибки'
-	params = models.TextField('Параметры')
-	output = models.TextField('Сообщение')
+    class Meta:
+        verbose_name = 'Ошибка'
+        verbose_name_plural = 'Ошибки'
+
+    params = models.TextField('Параметры')
+    output = models.TextField('Сообщение')
--- stats/views.py
+++ stats/views.py
@@ -2,146 +2,150 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from datetime import datetime, timedelta
 
-
 from .models import Game, TradePoint, Error, BulletHistory
 
-#INSERT INTO `Tir`(`Точка тира`, `Мишень`, `Дата`, `Время`, `Время игры`, `Статистика упавших`,
- #`Количество холостых`, `Приз`, `Прерывание`, `Служебные`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])
+# INSERT INTO `Tir`(`Точка тира`, `Мишень`, `Дата`, `Время`, `Время игры`, `Статистика упавших`,
+# `Количество холостых`, `Приз`, `Прерывание`, `Служебные`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8],[value-9],[value-10])
 
-#TT=63001000&Mishen=1&Date="2017-4-29"&Time="	15:16:33"&GameTime=18&
-#Stat="00000 00000 00000 00000"&Hol=3&Priz=0&Stop=0&Other=0
+# TT=63001000&Mishen=1&Date="2017-4-29"&Time="	15:16:33"&GameTime=18&
+# Stat="00000 00000 00000 00000"&Hol=3&Priz=0&Stop=0&Other=0
 
-trade_point, idle_count, fallen_count, interrupt = ('trade_point','idle_count', 'fallen_count', 'interrupt')
+trade_point, idle_count, fallen_count, interrupt = ('trade_point', 'idle_count', 'fallen_count', 'interrupt')
+
 
 def cleanInput(string):
-	return string.replace('"', '');
+    return string.replace('"', '');
+
+
 def getDataFromUrl(req):
-	get = lambda name, default: cleanInput(req.get(name, default))
-	result = {}
-	skipFields = ('Date', 'Time', 'TT')
-	mapUrlToProps = {
-		'Mishen': 'target',
-		'GameTime': 'duration',
-		'Stat': fallen_count,
-		'Hol': idle_count,
-		'Priz': 'prise',
-		'Stop': 'interrupt',
-		'Other': 'service',
-	}
-	for key, val in mapUrlToProps.items():
-		if key in skipFields:
-			continue
-		result[val] = get(key, '')
+    get = lambda name, default: cleanInput(req.get(name, default))
+    result = {}
+    skipFields = ('Date', 'Time', 'TT')
+    mapUrlToProps = {
+        'Mishen': 'target',
+        'GameTime': 'duration',
+        'Stat': fallen_count,
+        'Hol': idle_count,
+        'Priz': 'prise',
+        'Stop': 'interrupt',
+        'Other': 'service',
+    }
+    for key, val in mapUrlToProps.items():
+        if key in skipFields:
+            continue
+        result[val] = get(key, '')
 
-	#return result
-	result[trade_point] = TradePoint.objects.get(label=get('TT', ''))
-	result[interrupt] = result[interrupt] == '0'
+    # return result
+    result[trade_point] = TradePoint.objects.get(label=get('TT', ''))
+    result[interrupt] = result[interrupt] == '0'
 
-	dateFormat = '%Y-%m-%dT%H:%M:%S'
-	result['start_time'] = datetime.strptime(
-		get('Date','') + 'T' + get('Time', ''),
-		dateFormat
-	)
+    dateFormat = '%Y-%m-%dT%H:%M:%S'
+    result['start_time'] = datetime.strptime(
+        get('Date', '') + 'T' + get('Time', ''),
+        dateFormat
+    )
 
-	result['duration']  = timedelta(minutes=int(result['duration']))
-	return result
+    result['duration'] = timedelta(minutes=int(result['duration']))
+    return result
 
 
 def insert(request):
-	try:
-		params = getDataFromUrl(request.GET)
-		g = Game(**params)
-		g.save()
-		tp = params[trade_point]
-		bh=BulletHistory(trade_point = tp, bullet_count  = -(20 + int(params[idle_count])))
-		bh.save()
-		results = params[fallen_count]
-		if results.startswith('1'*20):
-			tp.big_toys_count -= 1
-		elif results.startswith('1'*15):
-			tp.small_toys_count -= 1
-		tp.save()
-		return HttpResponse("success")
-	except Exception as e:
-		error = Error(params=request.META['QUERY_STRING'], output=repr(e))
-		error.save()
-		return HttpResponse("error")
+    try:
+        params = getDataFromUrl(request.GET)
+        g = Game(**params)
+        g.save()
+        tp = params[trade_point]
+        bh = BulletHistory(trade_point=tp, bullet_count=-(20 + int(params[idle_count])))
+        bh.save()
+        results = params[fallen_count]
+        if results.startswith('1' * 20):
+            tp.big_toys_count -= 1
+        elif results.startswith('1' * 15):
+            tp.small_toys_count -= 1
+        tp.save()
+        return HttpResponse("success")
+    except Exception as e:
+        error = Error(params=request.META['QUERY_STRING'], output=repr(e))
+        error.save()
+        return HttpResponse("error")
 
 
-def showTradePointStats(request,id):
-	return render(request, 'stats/tradepoint_select_params.html', {'id':id})
+def showTradePointStats(request, id):
+    return render(request, 'stats/tradepoint_select_params.html', {'id': id})
+
 
 def getFilterDate(period):
-	relation = {
-		'month': 30, 'week': 7, 'day': 1
-	}
-	daysCount = relation[period]
-	return datetime.today() - timedelta(days=daysCount)
+    relation = {
+        'month': 30, 'week': 7, 'day': 1
+    }
+    daysCount = relation[period]
+    return datetime.today() - timedelta(days=daysCount)
+
 
 def getModel(_type):
-	relation = {
-		'game': Game, 'bullet': BulletHistory,'prise': Game
-	}
-	return relation[_type]
+    relation = {
+        'game': Game, 'bullet': BulletHistory, 'prise': Game
+    }
+    return relation[_type]
+
 
 def plotGame(data):
-	import matplotlib as mpl
-	mpl.use('Agg')
-	import matplotlib.pyplot as pl
-	from collections import Counter
-	import io
-	c = Counter(x[0] for x in data)
-	fig = pl.figure()
-	ax = fig.add_subplot(111)
-	ax.bar(c.keys(), c.values(), width=.1)
-	#ax.bar(['foo','bar'], [1,5], width=.1)
-	#ax.xaxis_date()
-	buf = io.BytesIO()
-	fig.autofmt_xdate()
-	fig.savefig(buf, format='png')
-	buf.seek(0)
-	v = buf.getvalue()
-	buf.close()
-	return v
+    import matplotlib as mpl
+    mpl.use('Agg')
+    import matplotlib.pyplot as pl
+    from collections import Counter
+    import io
+    c = Counter(x[0] for x in data)
+    fig = pl.figure()
+    ax = fig.add_subplot(111)
+    ax.bar(c.keys(), c.values(), width=.1)
+    # ax.bar(['foo','bar'], [1,5], width=.1)
+    # ax.xaxis_date()
+    buf = io.BytesIO()
+    fig.autofmt_xdate()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    v = buf.getvalue()
+    buf.close()
+    return v
+
 
 def plotBullets(data):
-	import matplotlib as mpl
-	mpl.use('Agg')
-	import matplotlib.pyplot as pl
-	from collections import Counter
-	import io
-	d = dict()
-	for (date, instance) in data:
-		if date not in d:
-			d[date] = 0
-		d[date] += -(instance.bullet_count)
+    import matplotlib as mpl
+    mpl.use('Agg')
+    import matplotlib.pyplot as pl
+    from collections import Counter
+    import io
+    d = dict()
+    for (date, instance) in data:
+        if date not in d:
+            d[date] = 0
+        d[date] += -(instance.bullet_count)
 
-	fig = pl.figure()
-	ax = fig.add_subplot(111)
-	ax.bar(d.keys(), d.values(), width=.1)
+    fig = pl.figure()
+    ax = fig.add_subplot(111)
+    ax.bar(d.keys(), d.values(), width=.1)
 
-	buf = io.BytesIO()
-	fig.autofmt_xdate()
-	fig.savefig(buf, format='png')
-	buf.seek(0)
-	v = buf.getvalue()
-	buf.close()
-	print(data)
-	return v
+    buf = io.BytesIO()
+    fig.autofmt_xdate()
+    fig.savefig(buf, format='png')
+    buf.seek(0)
+    v = buf.getvalue()
+    buf.close()
+    print(data)
+    return v
+
 
 plots = {'game': plotGame, 'bullet': plotBullets, 'prise': plotGame}
 
 
-
-
-
 def tradePointStats(request, id):
-	period = getFilterDate( request.GET.get('period') )
-	params = {'trade_point': id, 'start_time__gte': period}
-	_type = request.GET.get('type')
-	if _type == 'prise':
-		params['prise'] = True
-	objects = getModel(_type).objects.filter(**params)
-	result = tuple((g.start_time.strftime("%d %h %H:%M"), g) for g in objects)
-	file = plots[_type](result)
-	return HttpResponse(file, content_type='image/png')
+    period = getFilterDate(request.GET.get('period'))
+    params = {'trade_point': id, 'start_time__gte': period}
+    _type = request.GET.get('type')
+    if _type == 'prise':
+        params['prise'] = True
+    objects = getModel(_type).objects.filter(**params)
+    result = tuple((g.start_time.strftime("%d %h %H:%M"), g) for g in objects)
+    file = plots[_type](result)
+    return HttpResponse(file, content_type='image/png')
