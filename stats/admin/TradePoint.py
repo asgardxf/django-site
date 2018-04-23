b8ab4d0f10873e9fe5388c12dd6c334d983272eb
Dmitriy Zuykov <dmitriy.zuykov@altarix.org>
2018-04-23 17:57:03 +0400

    issue-9

--- stats/admin/TradePoint.py
+++ stats/admin/TradePoint.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.utils.html import format_html
+from django.db.models.expressions import RawSQL
+import datetime
 
 from ..models import TradePoint, Operator, Game
 
@@ -37,3 +39,58 @@ class TradePointAdmin(admin.ModelAdmin):
         return format_html('<a href="/showStats/trade_point/{}" target="_blank">Статистика</a>', instance.id)
 
     stats.short_description = 'Статистика'
+
+
+class MonitoringModel(TradePoint):
+    class Meta:
+        proxy = True
+        verbose_name = 'Мониторинг'
+        verbose_name_plural = 'Мониторинг'
+
+
+@admin.register(MonitoringModel)
+class Monitoring(admin.ModelAdmin):
+    list_display = ('name', 'game_count', 'correct_game_count', 'bullets', 'prise')
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+    def get_queryset(self, request):
+        today = str(datetime.date.today())
+        qs = TradePoint.objects.annotate(
+            game_count=RawSQL(
+                'select count(*) from stats_game g '
+                'where g.trade_point_id = stats_tradepoint.id and g.start_time > date(%s)', [today]
+            ),
+            correct_game_count=RawSQL(
+                'select count(*) from stats_game g '
+                'where g.trade_point_id = stats_tradepoint.id and g.start_time > date(%s) and not interrupt', [today]
+            ),
+            bullets=RawSQL(
+                'select sum(bullet_count) from stats_bullethistory b '
+                'where b.trade_point_id = stats_tradepoint.id and b.start_time > date(%s)',
+                [today]
+            ),
+            prise=RawSQL(
+                'select sum(1) from stats_game g '
+                'where g.trade_point_id = stats_tradepoint.id and g.start_time > date(%s) and prise', [today]
+            ),
+        )
+        return qs
+
+    def game_count(self, obj):
+        return obj.game_count
+    game_count.short_description = 'Всего игр'
+
+    def correct_game_count(self, obj):
+        return obj.correct_game_count
+    correct_game_count.short_description = 'Завершённых игр'
+
+    def bullets(self, obj):
+        return obj.bullets
+    bullets.short_description = 'Пули'
+
+    def prise(self, obj):
+        return obj.prise
+    prise.short_description = 'Призы'
